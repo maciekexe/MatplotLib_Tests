94c298c995f72543237b3eca7c13e3b8ca2b21dc
KarolS3 <karol.stolc3@gmail.com>
2026-05-06 19:17:24 +0200

    feat: add test structure for dashboard

--- src/functional_tests/test_complex_dashboard.py
+++ src/functional_tests/test_complex_dashboard.py
@@ -4,3 +4,5 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import os
+def test_complex_dashboard(tmp_path):
+    output_file = tmp_path / "dashboard.png"
