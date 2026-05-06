1ac9b01a8d53be34322942063d7784b237c2d338
KarolS3 <karol.stolc3@gmail.com>
2026-05-06 19:19:34 +0200

    feat: add plotting logic for dashboard test

--- src/functional_tests/test_complex_dashboard.py
+++ src/functional_tests/test_complex_dashboard.py
@@ -6,3 +6,13 @@ from matplotlib.gridspec import GridSpec
 import os
 def test_complex_dashboard(tmp_path):
     output_file = tmp_path / "dashboard.png"
+    fig = plt.figure(figsize=(10, 8))
+    gs = GridSpec(2, 2, figure=fig)
+
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax3 = fig.add_subplot(gs[1, :])
+
+    ax1.plot([1, 2, 3], [1, 4, 9])
+    ax2.bar(["A", "B", "C"], [5, 3, 7])
+    ax3.scatter([1, 2, 3], [3, 1, 4])
