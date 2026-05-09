e0543675121dac032b71208566fc7687f660c4a8
NatanielZ <natecki2008@gmail.com>
2026-05-09 18:36:56 +0200

    feat: zapis wyników testu DPI do pliku log

--- src/performance_tests/test2.py
+++ src/performance_tests/test2.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+
 def render_plot(dpi, filename):
     x = np.linspace(0, 50, 10000)
     y = np.cos(x)
@@ -16,4 +17,13 @@ def render_plot(dpi, filename):
     plt.savefig(filename)
     plt.close()
 
- return time.time() - start
+    return time.time() - start
+
+
+def test_dpi_render_performance(tmp_path):
+    low = render_plot(100, tmp_path / "low.png")
+    high = render_plot(600, tmp_path / "high.png")
+
+    with open(tmp_path / "dpi.log", "w") as f:
+        f.write(f"100 DPI: {low}\n")
+        f.write(f"600 DPI: {high}\n")
