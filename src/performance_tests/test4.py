eb380d6073d63fd9ec2fc33f4a5481a351ad8ee5
NatanielZ <natecki2008@gmail.com>
2026-05-09 19:33:38 +0200

    feat: zapis wyników eksportu do pliku log

--- src/performance_tests/test4.py
+++ src/performance_tests/test4.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+
 def export(format, filename):
     x = np.linspace(0, 100, 20000)
     y = np.sin(x)
@@ -17,3 +18,12 @@ def export(format, filename):
     plt.close()
 
     return time.time() - start
+
+
+def test_export_format_performance(tmp_path):
+    png_time = export("png", tmp_path / "plot.png")
+    svg_time = export("svg", tmp_path / "plot.svg")
+
+    with open(tmp_path / "export.log", "w") as f:
+        f.write(f"PNG: {png_time}\n")
+        f.write(f"SVG: {svg_time}\n")
