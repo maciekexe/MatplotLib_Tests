48eb9aa80ff4ccd47dbf1a88eda319307ae89479
NatanielZ <natecki2008@gmail.com>
2026-05-09 18:31:24 +0200

    feat: implementacja funkcji renderującej wykres z parametrem DPI

--- src/performance_tests/test2.py
+++ src/performance_tests/test2.py
@@ -4,3 +4,16 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
+def render_plot(dpi, filename):
+    x = np.linspace(0, 50, 10000)
+    y = np.cos(x)
+
+    start = time.time()
+
+    plt.figure(dpi=dpi)
+    plt.plot(x, y)
+    plt.savefig(filename)
+    plt.close()
+
+    return time.time() - start
