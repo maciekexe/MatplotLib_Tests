249792fd1b48fbd66a79cbdfb253933ad8d946ec
NatanielZ <natecki2008@gmail.com>
2026-05-09 19:22:28 +0200

    feat: implementacja generowania wielu subplotów

--- src/performance_tests/test3.py
+++ src/performance_tests/test3.py
@@ -4,3 +4,15 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
+def create_subplots(n, filename):
+    start = time.time()
+
+    fig, axes = plt.subplots(n, n, figsize=(6, 6))
+
+    for ax in axes.flat:
+        x = np.linspace(0, 10, 100)
+        ax.plot(x, np.sin(x))
+
+    plt.savefig(filename)
+    plt.close()
